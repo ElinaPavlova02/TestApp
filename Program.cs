5d099d2199d1ad2a5953f74173e742803e12f1a6
ElinaPavlova02 <66823861+ElinaPavlova02@users.noreply.github.com>
2021-07-17 16:42:43 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+using System;
 using System.Threading.Tasks;
 
 namespace TestApp
@@ -12,30 +11,37 @@ namespace TestApp
         /// <param name="array"></param>
         public static void Reverse(byte[] array)
         {
-            foreach (byte element in array)
+            for (int i = 0; i < array.Length; i++)
             {
-                string tmp = String.Format("{0:d8}", Convert.ToInt32(Convert.ToString(element, 2)));
+                int tmp = array[i];
 
-                string reverseTmp = new string(tmp.ToCharArray().Reverse().ToArray());
+                tmp = ((tmp & 0x55) << 1) | ((tmp >> 1) & 0x55); // Поменять местами соседние биты
+                tmp = ((tmp & 0x33) << 2) | ((tmp >> 2) & 0x33); // Поменять местами соседние пары бит
+                tmp = ((tmp & 0x0F) << 4) | ((tmp >> 4) & 0x0F); // Поменять местами первые и последние 4 бита
 
-                int reverseElement = Convert.ToInt32(reverseTmp, 2);
-
-                Console.WriteLine(reverseElement);
+                Console.WriteLine(tmp);
+                
+                array[i] = (byte)tmp;
             }
         }
 
         static async Task Main(string[] args)
         {
             // Проверка работы класса UserRepository
-           
-            var user = await UserRepository.GetUserAsync(1);
 
-            Console.WriteLine(user);
+            UserRepository repository = new UserRepository();
 
-            UserRepository.AddUserAsync(5, "John");
+            var u = await repository.GetUserAsync(1);
 
-            await UserRepository.GetOrderedUsersAsync();
+            Console.WriteLine(u);
 
+            User user = new User { Id = 10, Name = "Michael"};
+
+            repository.AddUserAsync(user);
+
+            Console.WriteLine();
+
+            await repository.GetOrderedUsersAsync();
 
             Console.WriteLine();
 
